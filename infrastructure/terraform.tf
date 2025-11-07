f1b343e4b0a5c05944ce94574eebd6381396cbb5
sophieperryy-3 <sophie.perry@uk.ey.com>
2025-11-07 09:28:46 +0000

    Fix Terraform: Add random provider for S3 bucket naming

--- infrastructure/terraform.tf
+++ infrastructure/terraform.tf
@@ -5,9 +5,15 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
   }
 }
 
 provider "aws" {
   region = var.aws_region
 }
+
+provider "random" {}
